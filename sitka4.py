f23ccdbefc5b776225ddc63ed915f8c9c8cea0a9
James Sutton <James_suttonjr1@baylor.edu>
2021-09-29 16:30:02 -0500

    sitka 4 file and death valley file

--- sitka4.py
+++ sitka4.py
@@ -3,7 +3,7 @@ import csv
 from datetime import datetime
 
 
-open_file = open("sitka_weather_2018_simple.csv", "r")
+open_file = open("death_valley_2018_simple.csv", "r")
 
 csv_file = csv.reader(open_file, delimiter=",")
 
@@ -15,8 +15,6 @@ for index, column_header in enumerate(header_row):
     print(index, column_header)
 
 # testing to convert date from string
-mydate = datetime.strptime('2018-07-01', '%Y-%m-%d')
-print(mydate)
 
 
 highs = []
@@ -25,15 +23,21 @@ lows = []
 
 
 for row in csv_file:
-    highs.append(int(row[5]))
-    the_date = datetime.strptime(row[2], '%Y-%m-%d')
-    dates.append(the_date)
-    lows.append(int(row[6]))
+    try:
+        the_date = datetime.strptime(row[2], '%Y-%m-%d')
+        high = int(row[4])
+        low = int(row[5])
+    except ValueError:
+        print(f"Missing data for {the_date}")
+    else:
+        highs.append(int(row[4]))
+        dates.append(the_date)
+        lows.append(int(row[5]))
+# print(highs)
+# print(dates)
+# print(lows)
 
-print(highs)
-print(dates)
-print(lows)
-
+'''
 fig = plt.figure()
 
 plt.title("Daily high temperatures, July 2018", fontsize=16)
@@ -63,3 +67,5 @@ plt.title("Lows")
 plt.suptitle("Highs and Lows of Sitka, Alaska")
 
 plt.show()
+
+'''
